040473c5e25c1b9a08959dd727a9c1eabd2d4049
LloydLion <55992792+LloydLion@users.noreply.github.com>
2020-11-28 23:51:16 +0300

    Добавьте файлы проекта.

---
+++ CGZDiscordBot/Program.cs
@@ -0,0 +1,29 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CGZDiscordBot
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var client = new DiscordSocketClient();
+
+			client.Log += (m) => { Console.WriteLine(m.Message); return Task.CompletedTask; };
+			client.MessageReceived += Client_MessageReceived;
+
+			client.LoginAsync(TokenType.Bot, @"NzgyMzQ0MDA1OTUzMzg4NTg0.X8K0og.xnmh5esDi21KrQRPiN1IQkYc2Wk").ContinueWith((s) => client.StartAsync());
+			Thread.Sleep(-1);
+		}
+
+		private static Task Client_MessageReceived(SocketMessage arg)
+		{
+
+
+			return Task.CompletedTask;
+		}
+	}
+}
